f83ed104f713d67ad666bc394fc77921484e3f77
abeer hafez <abeerhafez418@gmail.com>
2024-10-05 00:17:57 +0300

    angular testing

--- src/app/lab/hero service/hero.lab.service.integration.spec.ts
+++ src/app/lab/hero service/hero.lab.service.integration.spec.ts
@@ -1,5 +1,63 @@
-describe("3-hero service (http) integration testing:", () => {
-    
-    it("getHeroes function: send request and receive response successfully", () => { })
-    it("updateHero function: send request and receive response successfully", () => { })
-})
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HeroServiceForLab } from './hero.lab.service';
+import { Hero } from '../../hero';
+
+describe('3-hero service (http) integration testing:', () => {
+  let heroService: HeroServiceForLab;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HeroServiceForLab],
+    });
+
+    heroService = TestBed.inject(HeroServiceForLab);
+    httpTestingController = TestBed.inject(HttpTestingController); 
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('getHeroes function: send request and receive response successfully', () => {
+    const expectedHeroes: Hero[] = [
+      {
+        id: 1,
+        name: 'Superman',
+      },
+      {
+        id: 2,
+        name: 'Batman',
+      },
+    ];
+
+    heroService.getHeroes().subscribe((heroes) => {
+      expect(heroes).toEqual(expectedHeroes);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3000/heroes');
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedHeroes);
+  });
+
+  it('updateHero function: send request and receive response successfully', () => {
+    const updatedHero: Hero = {
+      id: 1,
+      name: 'Superman Updated',
+    };
+
+    heroService.updateHero(updatedHero).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3000/heroes');
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body).toEqual(updatedHero);
+    req.flush(updatedHero);
+  });
+});
--- src/app/lab/messages/messages.lab.component.spec.ts
+++ src/app/lab/messages/messages.lab.component.spec.ts
@@ -1,10 +1,44 @@
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { MessagesComponentForLab } from './messages.lab.component';
+import { MessageService } from '../../services/message/message.service';
+import { By } from '@angular/platform-browser';
 
 describe("2-message component integration testing:", () => {
-   
-   
+  let fixture: ComponentFixture<MessagesComponentForLab>;
+  let messageService: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MessagesComponentForLab],
+      providers: [MessageService]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MessagesComponentForLab);
+    messageService = TestBed.inject(MessageService);
+  });
     it("expect component template to be empty", () => {
-        //Note: there is @if"messageService.messages.length" in line 1 in template
+
+        messageService.messages = [];
+
+        fixture.detectChanges();
+
+        const messageElements = fixture.debugElement.queryAll(By.css('.msg'));
+        expect(messageElements.length).toBe(0);
     })
-    it("then expect div.msg to have the messages after setting it", () => {})
-})
+
+    
+    it("then expect div.msg to have the messages after setting it", () => {
+
+      messageService.messages = [
+        { id: 1, message: 'Test Message 1' },
+        { id: 2, message: 'Test Message 2' }
+      ];
+
+      fixture.detectChanges();
+
+      const messageElements = fixture.debugElement.queryAll(By.css('.msg'));
+      expect(messageElements.length).toBe(2);
+
+      expect(messageElements[0].nativeElement.textContent).toContain('Test Message 1');
+    });
+    })
